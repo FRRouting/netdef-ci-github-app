649a26b48f286984ac340b3b8935302d33c8651a
Rodrigo Nardi <rnardi@netdef.org>
2023-12-07 22:40:49 -0300

    Ci Job Factory
    Updating factory
    
    Signed-off-by: Rodrigo Nardi <rnardi@netdef.org>

--- spec/factories/ci_job.rb
+++ spec/factories/ci_job.rb
@@ -13,7 +13,6 @@ FactoryBot.define do
     name { Faker::App.name }
     status { 0 }
     job_ref { Faker::Alphanumeric.alphanumeric(number: 18, min_alpha: 3, min_numeric: 3) }
-    check_ref { rand(1_000_000) }
 
     check_suite
 
