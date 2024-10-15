18bd57e920a9ab2554c9edd8b9a25b2dcf1e0507
Rodrigo Nardi <rnardi@netdef.org>
2024-10-15 14:43:50 -0300

    Console - GitHub / Slack
    Adding details about slack account
    
    Signed-off-by: Rodrigo Nardi <rnardi@netdef.org>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_09_102350) do
+ActiveRecord::Schema[7.2].define(version: 2024_10_14_134659) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -108,6 +108,8 @@ ActiveRecord::Schema[7.2].define(version: 2024_10_09_102350) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "organization_id"
+    t.string "slack_username"
+    t.string "slack_id"
     t.index ["github_id"], name: "index_github_users_on_github_id", unique: true
     t.index ["organization_id"], name: "index_github_users_on_organization_id"
   end
